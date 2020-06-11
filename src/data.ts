c79e72435cfd9683e9a7a287f3c72ac345f9e73e
Egor <wadjetz@tuta.io>
2020-06-11 19:13:12 +0200

    wip

--- src/data.ts
+++ src/data.ts
@@ -26,9 +26,61 @@ export const cvData: CvData = {
       freelance: true,
       company: "Zeenea",
       place: "Paris (Remote)",
-      startDate: new Date(2020, 3, 14),
-      endDate: new Date(2020, 5, 30),
+      startDate: new Date(2020, 3, 1),
+      endDate: new Date(2020, 5, 1),
       technologies: ["Scala", "Typescript"]
+    },
+    {
+      title: {
+        fr: "Full Stack Developer",
+        en: "Full Stack Developer",
+      },
+      description: {
+        fr: "Development of the bank as a service system with microservices and event streaming",
+        en: "Development of the bank as a service system with microservices and event streaming",
+      },
+      freelance: true,
+      company: "Ditto Service",
+      place: "Neuilly-sur-Seine",
+      startDate: new Date(2020, 0, 1),
+      endDate: new Date(2020, 2, 1),
+      technologies: ["Scala", "Typescript", "Kafka", "PostgreSQL", "React"]
+    },
+    {
+      title: {
+        fr: "Full-stack Developer",
+        en: "Full-stack Developer",
+      },
+      description: {
+        fr: "Full stack developer with Scala and React. Developer of the native mobile SDK with kotlin and swift",
+        en: "Full stack developer with Scala and React. Developer of the native mobile SDK with kotlin and swift",
+      },
+      freelance: false,
+      company: "ReachFive",
+      place: "Paris, France",
+      startDate: new Date(2018, 10, 1),
+      endDate: new Date(2019, 11, 1),
+      technologies: ["Scala", "Typescript", "React", "Swift", "Kotlin", "Android", "iOS"],
+    },
+    {
+      title: {
+        fr: "Software Architect",
+        en: "Software Architect",
+      },
+      description: {
+        fr: `Web developer with Scala, Play Framework, Typescript, React, Redux, React-native.
+        - Nestincloud : the cloud platform that lets you build, deploy, run & operate your apps easily.
+        - Nomad Mobile : react-native mobile application- MonEspaceUp: react-native mobile application`,
+        en: `Web developer with Scala, Play Framework, Typescript, React, Redux, React-native.
+        - Nestincloud : the cloud platform that lets you build, deploy, run & operate your apps easily.
+        - Nomad Mobile : react-native mobile application- MonEspaceUp: react-native mobile application`
+      },
+      freelance: false,
+      company: "Zengularity (Fabernovel)",
+      place: "Paris, France",
+      startDate: new Date(2014, 9, 1),
+      endDate: new Date(2018, 10, 1),
+      technologies: ["Scala", "Typescript", "React", "React Native", "Play Framework", "Redux", "iOS"],
     }
   ],
   studies: [
