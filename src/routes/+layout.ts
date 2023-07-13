1d22d93b4b28fb74ee7ae7cc32d843eb028fb276
Egor Berezovskiy <egor@louveinvest.com>
2023-07-13 13:59:48 +0200

    Disable prerender (#29)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,8 +2,6 @@ import { loadLocaleAsync } from "$lib/i18n/i18n-util.async"
 
 import type { LayoutLoad } from "./$types"
 
-export const prerender = true
-
 export const load = (async event => {
   await loadLocaleAsync(event.data.locale)
   return event.data as App.Locals
