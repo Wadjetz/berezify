4b99681666bcbabc7b46264eec5d6de1ee8ece43
Egor Berezovskiy <egor@berezify.fr>
2025-12-04 22:12:44 +0100

    Upgrade dependencies (#57)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config"
 import { sveltekit } from "@sveltejs/kit/vite"
 import Icons from "unplugin-icons/vite"
-import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [sveltekit(), Icons({ compiler: "svelte" })],
