7243f50942cfce1437879b2544cfcbce0e699ec5
Egor <egor@berezify.fr>
2026-02-20 21:37:28 +0100

    Remove brss project

--- src/lib/cv/data/Projects.ts
+++ src/lib/cv/data/Projects.ts
@@ -31,38 +31,5 @@ A cooking recipes application
       "html",
       "redis"
     ]
-  },
-  {
-    title: "Mindstream",
-    description: {
-      fr: `
-- Une application de flux RSS
-- Gestionnaire de favoris des sites web
-- Gestionnaire de projets
-- Stackage de fichiers
-        `,
-      en: `
-- A RSS feed application
-- Web sites bookmarks manager
-- Projects manager
-- Files stackage
-      `
-    },
-    link: "https://brss.brocolix.fr",
-    linkLabel: "brss.brocolix.fr",
-    technologies: [
-      "typescript",
-      "svelte",
-      "sveltekit",
-      "rust",
-      "postgreSql",
-      "githubActions",
-      "actix",
-      "meilisearch",
-      "docker",
-      "css",
-      "html",
-      "redis"
-    ]
   }
 ]
